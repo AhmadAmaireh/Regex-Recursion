1f96546e4da2d0afdbce05dde9d7b248f2c3ae39
AhmadAmaireh <a_amayreh@hotmail.com>
2022-05-14 01:50:41 +0300

    recursion

--- recursion.test.js
+++ recursion.test.js
@@ -1,9 +1,15 @@
 /* Write a function to do the division operation without using the built-in division*/
 
-function division(number, dividedBy){
+var x = 0;
+function division(n1, n2) {
     // Write you logic here.
-    
-    return;
+    if (n1 < n2)
+        return x;
+    else {
+        x++;
+        n1 = n1 - n2;
+        return div(n1, n2);
+    }
 }
 
 /* Write a function that implement Math.pow(x,n) but using recursion
@@ -12,9 +18,12 @@ pow(2,4) = 16
 */
 
 
-function pow(x,n){
+function pow(x, n) {
     // Write you logic here.
-    return;
+    if (n == 0) { return 1; }
+
+    else { return x * pow(x, n - 1); }
+
 }
 
 /* The Fibonacci Series is a numeric series starting with the integers 0 and 1. In this series,
@@ -23,9 +32,13 @@ the next integer is determined by summing the previous two. This gives us:
 Write a function that take n as parameter and return the nth element in the Fibonacci Series
 Example: n = 4 ==> 3, n= 0 ==> 0, n = 3 ==> 2 */
 
-function fibonacci(n){
+function fibonacci(n) {
     // Write you logic here.
-    return;
+    if (n == 0 || n == 1) { return n; }
+
+    else { return fibonacci(n - 1) + fibonacci(n - 2); }
+
+
 }
 
 /* Optional 
@@ -42,7 +55,7 @@ Example:
 Input: n = 3, k = 3
 Output: ["123", "132", "213", "231", "312", "321"] */
 
-function permutations(n,k){
+function permutations(n, k) {
     let arr = [];
     // Write you logic here. 
     return arr
@@ -51,18 +64,18 @@ function permutations(n,k){
 
 describe("Test division", () => {
     test("Return the division result", () => {
-        expect(division(10,2)).toStrictEqual(5);
-        expect(division(10,0)).toStrictEqual(0);
-        expect(division(0,10)).toStrictEqual(0);
-        
+        expect(division(10, 2)).toStrictEqual(5);
+        expect(division(10, 0)).toStrictEqual(0);
+        expect(division(0, 10)).toStrictEqual(0);
+
     })
 });
 
 describe("Test pow", () => {
     test("It should work as Math.pow(x,n)", () => {
-        expect(pow(10,2)).toStrictEqual(100);
-        expect(pow(10,0)).toStrictEqual(1);
-        expect(pow(0,0)).toStrictEqual(1);
+        expect(pow(10, 2)).toStrictEqual(100);
+        expect(pow(10, 0)).toStrictEqual(1);
+        expect(pow(0, 0)).toStrictEqual(1);
     })
 });
 
@@ -78,7 +91,7 @@ describe("Test fibonacci", () => {
 
 describe("Test permutations", () => {
     test("It should return a list of possible combinations", () => {
-        expect(permutations(3,3)).toStrictEqual(["123", "132", "213", "231", "312", "321"]);
-        expect(permutations(3,0)).toStrictEqual([]);
+        expect(permutations(3, 3)).toStrictEqual(["123", "132", "213", "231", "312", "321"]);
+        expect(permutations(3, 0)).toStrictEqual([]);
     })
 });
